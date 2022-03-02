895290f6b00952d495ab6eb44c7807961563aee3
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-02 21:50:49 +0530

    Add constants

--- app/src/main/java/com/everest/mydetails/Constants.kt
+++ app/src/main/java/com/everest/mydetails/Constants.kt
@@ -4,3 +4,7 @@ const val FIRST_NAME = "FIRST_NAME"
 const val AGE = "AGE"
 const val BIO = "BIO"
 const val PHONENUMBER = "PHONENUMBER"
+const val PHONE_NUMBER_TOAST_MESSAGE = "Phone number shouldn't be less than 10"
+const val ALL_FIELDS_REQUIRED_MESSAGE = "All fields are required"
+const val AGE_TOAST_MESSAGE = "Not a valid age. Max limit - 120"
+
