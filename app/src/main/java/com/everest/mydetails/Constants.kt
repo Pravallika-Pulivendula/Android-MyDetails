9a04d648eacec291f46cb6392bc370fc0307905b
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-08 12:27:00 +0530

    Add constants

--- app/src/main/java/com/everest/mydetails/Constants.kt
+++ app/src/main/java/com/everest/mydetails/Constants.kt
@@ -4,4 +4,4 @@ const val FIRST_NAME = "FIRST_NAME"
 const val AGE = "AGE"
 const val BIO = "BIO"
 const val PHONENUMBER = "PHONENUMBER"
-
+const val USER_DETAILS = "USER_DETAILS"
---
+++ app/src/main/java/com/everest/mydetails/UserDetails.kt
@@ -0,0 +1,4 @@
+package com.everest.mydetails
+
+class UserDetails {
+}
